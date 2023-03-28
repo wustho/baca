4e573223d3e9a6cf56e589affbc2cde77de2c695
Benawi Adha <benawiadha@gmail.com>
2023-03-28 14:35:16 +0700

    Add a test

--- tests/test_html_parser.py
+++ tests/test_html_parser.py
@@ -1,4 +1,5 @@
-from baca.utils.html_parser import split_html_to_segments, parse_html_to_segmented_md
+from baca.utils.html_parser import split_html_to_segments
+from baca.models import SegmentType
 
 HTML_TEST = """
 <html><head><title>The Dormouse's story</title></head>
@@ -16,7 +17,39 @@ and they lived at the bottom of a well.</p>
 """
 
 
-# TODO:
 def test_html_splitters():
-    segments = split_html_to_segments(HTML_TEST, "test.html")
-    assert True
+    segments_iterator = split_html_to_segments(HTML_TEST, "test.html")
+
+    segment = next(segments_iterator)
+    assert segment.type == SegmentType.BODY
+    assert (
+        segment.content
+        == '<body> <p class="title"><b>The Dormouse\'s story</b></p> <p class="story">Once upon a time there were three little sisters; and their names were '
+    )
+    assert segment.nav_point == "test.html"
+
+    segment = next(segments_iterator)
+    assert segment.type == SegmentType.BODY
+    assert segment.content == '<a class="sister" href="http://example.com/elsie" id="link1">Elsie</a>,   '
+    assert segment.nav_point == "test.html#link1"
+
+    segment = next(segments_iterator)
+    assert segment.type == SegmentType.BODY
+    assert (
+        segment.content
+        == '<a class="sister" href="http://example.com/lacie" id="link2">Lacie</a> and and they lived at the bottom of a well.</p> '
+    )
+    assert segment.nav_point == "test.html#link2"
+
+    segment = next(segments_iterator)
+    assert segment.type == SegmentType.IMAGE
+    assert segment.content == "img_girl.jpg"
+    assert segment.nav_point == None
+
+    segment = next(segments_iterator)
+    assert segment.type == SegmentType.BODY
+    assert (
+        segment.content
+        == '<img alt="Girl in a jacket" height="600" src="img_girl.jpg" width="500"/> <p class="story">...</p> </body>'
+    )
+    assert segment.nav_point == None
