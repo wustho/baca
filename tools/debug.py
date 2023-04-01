b61a31a59b310c87c848e4dcfc7e9edc1f901180
Benawi Adha <benawiadha@gmail.com>
2023-04-01 16:00:42 +0700

    Update debugger

--- tools/debug.py
+++ tools/debug.py
@@ -2,12 +2,14 @@ from pathlib import Path
 
 from baca.app import Baca
 from baca.utils.cli import get_ebook_class
+from baca.utils.queries import get_last_read_ebook
 
 
 class Debug(Baca):
     def __init__(self):
         # file = Path("tmp/alice.azw3")
-        file = Path("tmp/alice.epub")
+        file = get_last_read_ebook()
+        assert file is not None
         # file = Path("tmp/andy.epub")
         # file = Path("tmp/frankenstein.older.mobi")
         # file = Path("tmp/frankenstein.mobi")
